fddfa266a61aa14075fe4a50a6616c092197e846
wyozi <joonasmertanen@gmail.com>
2014-11-24 13:28:17 +0200

    Retain search on page refresh as well now

--- js/controllers/searchController.js
+++ js/controllers/searchController.js
@@ -1,15 +1,27 @@
 angular.module('gmusicPlayerApp')
-    .controller('SearchCtrl', ['$scope', '$rootScope', 'GMusic', function($scope, $rootScope, GMusic) {
+    .controller('SearchCtrl', ['$scope', '$rootScope', '$location', 'GMusic', function($scope, $rootScope, $location, GMusic) {
         $scope.search = function() {
             var query = $scope.query;
-
-            GMusic.search(query, function(results) {
-                var onlyTracks = results.filter(function(obj) {
-                    return obj.type == "track";
-                }).map(function(obj) {
-                    return obj.track;
-                });
-                $rootScope.$broadcast('musicquery:setresults', 'search "' + query + '"', onlyTracks);
-            });
+            $location.path('/search/' + query);
         }
+
+        $scope.$watch(function() {
+            return $location.path();
+        }, function() {
+            var searchRegex = /\/search\/(.+)/;
+            var match = $location.path().match(searchRegex);
+
+            if (match) {
+                var query = match[1];
+
+                GMusic.search(query, function(results) {
+                    var onlyTracks = results.filter(function(obj) {
+                        return obj.type == "track";
+                    }).map(function(obj) {
+                        return obj.track;
+                    });
+                    $rootScope.$broadcast('musicquery:setresults', 'search "' + query + '"', onlyTracks);
+                });
+            }
+        });
     }]);
